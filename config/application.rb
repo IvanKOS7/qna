c2ce12ac77695a65706c4adc92dc4497ecff57c6
IvanKOS7 <ivankos429@gmail.com>
2022-11-13 13:34:28 +0700

    ac2

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 require "action_cable"
 require "rails/all"
 require 'sprockets/railtie'
+require "active_storage/engine"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
